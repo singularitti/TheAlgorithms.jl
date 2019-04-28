12b58059fffd446e96ec795d43129c642b3e3b9d
Qi Zhang <singularitti@outlook.com>
2019-04-28 19:33:52 -0400

    Add project using PkgTemplates.jl

---
+++ src/TheAlgorithms.jl
@@ -0,0 +1,5 @@
+module TheAlgorithms
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using TheAlgorithms
+using Test
+
+@testset "TheAlgorithms.jl" begin
+    # Write your own tests here.
+end
